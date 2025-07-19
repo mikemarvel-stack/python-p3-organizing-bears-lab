70ba8d03bb7fcd312bdf6f1a65bc31d2d8a94c62
michael <michaelmuturi15@gmail.com>
2025-07-19 17:29:20 +0300

    create.sql edited

---
+++ lib/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+('Mr. Chocolate', 15, 'M', 'brown', 'calm', 1),
+('Rowdy', 8, 'M', 'black', 'cool', 1),
+('Tabitha', 5, 'F', 'white', 'gentle', 1),
+('Sergeant Brown', 12, 'M', 'brown', 'grumpy', 0),

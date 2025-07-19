15b6eac546006c412a3dd22dc5eacc0c3bcadc8a
michael <michaelmuturi15@gmail.com>
2025-07-19 17:56:42 +0300

    final commit

--- lib/sql_queries.py
+++ lib/sql_queries.py
@@ -3,38 +3,37 @@ SELECT
     name,
     age
 FROM bears
-WHERE sex = 'F';
+WHERE sex='F';
 """
-
 select_all_bears_names_and_orders_in_alphabetical_order = """
-    SELECT
-        name
-    FROM bears
-    ORDER BY name ASC;
+SELECT
+    name
+FROM bears
+ORDER BY name ASC;
 """
 
 select_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest = """
-    SELECT
-        name,
-        age
-    FROM bears
-    WHERE alive = 1
-    ORDER BY age ASC;
+SELECT
+    name,
+    age
+FROM bears
+WHERE alive=1
+ORDER BY age ASC;
 """
 
 select_oldest_bear_and_returns_name_and_age = """
-    SELECT
-        name,
-        age
-    FROM bears
-    ORDER BY age DESC
-    LIMIT 1;
+SELECT
+    name,
+    age
+FROM bears
+ORDER BY age DESC
+LIMIT 1;
 """
 select_youngest_bear_and_returns_name_and_age = """
-    SELECT
-        name,
-        age
-    FROM bears
-    ORDER BY age ASC
-    LIMIT 1;
+SELECT
+    name,
+    age
+FROM bears
+ORDER BY age ASC
+LIMIT 1;
 """

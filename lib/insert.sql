5293a24c825b0dbcb89b0436abd4cb76534d5ba6
michael <michaelmuturi15@gmail.com>
2025-07-19 17:31:12 +0300

    insert.sql edited

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,3 +3,7 @@ INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
 ('Rowdy', 8, 'M', 'black', 'cool', 1),
 ('Tabitha', 5, 'F', 'white', 'gentle', 1),
 ('Sergeant Brown', 12, 'M', 'brown', 'grumpy', 0),
+('Melissa', 7, 'F', 'golden', 'playful', 1),
+('Grinch', 3, 'M', 'green', 'mischievous', 1),
+('Wendy', 6, 'F', 'blue', 'brave', 1),
+(NULL, 4, 'M', 'gray', 'shy', 0);
